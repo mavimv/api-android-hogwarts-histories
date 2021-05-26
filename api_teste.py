f3b9ad8217cc87b7ce00328271d8197894ba332d
Maria Vitoria <60761632+mavimv@users.noreply.github.com>
2021-05-25 23:01:58 -0300

    Update api_teste.py

--- api_teste.py
+++ api_teste.py
@@ -1,74 +1,69 @@
 from flask import Flask, jsonify, request
-import json
-import urllib.request
-import random
+
 
 app = Flask(__name__)
 
-usuario = [{"id": e, "nome": "Disciplina "+str(e), "ementa":"Ementa "+str(e), "foto":"https://cdn.pixabay.com/photo/2018/01/18/20/42/pencil-3091204_1280.jpg", "professor": "Professor Disciplina "+str(e)} for e in range(1,11)]   
+usuarios = [
+            {
+                "id": e,
+                "nome": "Usuario "+str(e),
+                "senha": str(e + 1)
+            }
+            for e in range(1, 11)
+           ]
 
-@app.route("/disciplinas", methods=['GET'])
+usuario =  {
+                "id": 12,
+                "nome": 'aluno',
+                "senha": "impacta"
+           }
+
+usuarios.append(usuario)
+
+
+@app.route("/usuarios", methods=['GET'])
 def get():
-    return jsonify(disciplinas)
+    return jsonify(usuarios)
 
-@app.route("/disciplinas/<int:id>", methods=['GET'])
+
+@app.route("/usuarios/<int:id>", methods=['GET'])
 def get_one(id):
-    filtro = [e for e in disciplinas if e["id"] == id]
+    filtro = [e for e in usuarios if e["id"] == id]
     if filtro:
         return jsonify(filtro[0])
     else:
         return jsonify({})
 
-@app.route("/disciplinas", methods=['POST'])
+
+@app.route("/usuarios", methods=['POST'])
 def post():
-    global disciplinas
+    global usuarios
     try:
         content = request.get_json()
-
-        # gerar id
-        ids = [e["id"] for e in disciplinas]
+        # gerar id auto
+        ids = [e["id"] for e in usuarios]
         if ids:
             nid = max(ids) + 1
         else:
             nid = 1
         content["id"] = nid
-        disciplinas.append(content)
-        return jsonify({"status":"OK", "msg":"disciplina adicionada com sucesso"})
+        usuarios.append(content)
+        return jsonify({"status": "OK",
+                        "msg": "usuario adicionado com sucesso"})
     except Exception as ex:
-        return jsonify({"status":"ERRO", "msg":str(ex)})
+        return jsonify({"status": "ERRO", "msg": str(ex)})
 
-@app.route("/disciplinas/<int:id>", methods=['DELETE'])
+
+@app.route("/usuarios/<int:id>", methods=['DELETE'])
 def delete(id):
-    global disciplinas
+    global usuarios
     try:
-        disciplinas = [e for e in disciplinas if e["id"] != id]
-        return jsonify({"status":"OK", "msg":"disciplina removida com sucesso"})
+        usuarios = [e for e in usuarios if e["id"] != id]
+        return jsonify({"status": "OK",
+                        "msg": "usuario removido com sucesso"})
     except Exception as ex:
-        return jsonify({"status":"ERRO", "msg":str(ex)})
-
-@app.route("/push/<string:key>/<string:token>", methods=['GET'])
-def push(key, token):
-	d = random.choice(disciplinas)
-	data = {
-		"to": token,
-		"notification" : {
-			"title":d["nome"],
-			"body":"Você tem nova atividade em "+d['nome']
-		},
-		"data" : {
-			"disciplinaId":d['id']
-		}
-	}
-	req = urllib.request.Request('http://fcm.googleapis.com/fcm/send')
-	req.add_header('Content-Type', 'application/json')
-	req.add_header('Authorization', 'key='+key)
-	jsondata = json.dumps(data)
-	jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-	req.add_header('Content-Length', len(jsondataasbytes))
-	response = urllib.request.urlopen(req, jsondataasbytes)
-	print(response)
-	return jsonify({"status":"OK", "msg":"Push enviado"})
+        return jsonify({"status": "ERRO", "msg": str(ex)})
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
